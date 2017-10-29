376bbdf4852b701c306b549252abea59c2d892d2
Andy <mingfun.wong.chn@gmail.com>
2017-10-29 16:42:25 +0900

    refine

--- apache_restart.cmd
+++ apache_restart.cmd
@@ -2,4 +2,4 @@
 cd /D %~dp0
 
 (sc query .apache | find ".apache">nul && net stop .apache)
-(sc query .mysql | find ".mysql">nul && net start .mysql)
+(sc query .apache | find ".apache">nul && net start .apache)
